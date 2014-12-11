d14747da5eae4b87e32a073f6a6784edd89e81a5
Deva Palanisamy <dev.palanisamy@gmail.com>
2014-12-11 20:01:03 +0000

    removed default comments in view controllers

--- Falcon/Falcon/ViewControllers/BooksViewController.m
+++ Falcon/Falcon/ViewControllers/BooksViewController.m
@@ -14,11 +14,12 @@
 
 @implementation BooksViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (instancetype)init
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+    self = [super init];
+    if (self)
+    {
+        self.title = @"Books";
     }
     return self;
 }
@@ -26,24 +27,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
